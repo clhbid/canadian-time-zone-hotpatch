294966b7b660a900a5b9a14871fd4cbd152869ee
Mark Bennett <mark@clhbid.com>
2026-09-21 15:03:21 -0600

    6: Align lint diagnostics with clhbid.com

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist/**", "node_modules/**", "coverage/**"] },
+  { linterOptions: { reportUnusedDisableDirectives: "error" } },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
